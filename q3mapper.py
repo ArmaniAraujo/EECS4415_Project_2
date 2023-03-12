2b850b011dc78f7517bd679d5aec779dd3bf88e3
Armani Araujo <44607139+armanidarren@users.noreply.github.com>
2023-03-12 16:57:10 -0400

    Working on q3 stuff

--- q3mapper.py
+++ q3mapper.py
@@ -1,2 +1,16 @@
 #!/usr/local/bin/python3.9
 
+import json
+
+counter = 0
+output = ''
+
+with open('yelp_academic_dataset_review.json', encoding='utf-8') as f:
+
+    for jsonObj in f:
+        tempObj = json.loads(jsonObj)
+        ufc = int(tempObj['useful']) + int(tempObj['funny']) + int(tempObj['cool'])
+        print(tempObj['review_id'], '\t', ufc, '\t', tempObj['date'], sep='')
+
+
+
--- q3reducer.py
+++ q3reducer.py
@@ -1,2 +1,8 @@
 #!/usr/local/bin/python3.9
 
+import sys
+
+
+dict = {'a':4, 'b':3, 'c':5, 'd':12}
+
+print(sorted(dict))
